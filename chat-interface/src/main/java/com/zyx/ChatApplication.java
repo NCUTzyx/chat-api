d08bf5a301db9c89f36b199b109dcda32c56d04d
zhang yusen <2153844418@qq.com>
2024-05-16 23:12:22 +0800

    feat:init ddd project

---
+++ chat-interface/src/main/java/com/zyx/ChatApplication.java
@@ -0,0 +1,20 @@
+package com.zyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangyusen
+ * @version 1.0
+ * @description: 启动文件
+ * @date 2024/5/16 22:22
+ */
+
+@SpringBootApplication
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class,args);
+
+    }
+}
---
+++ chat-interface/src/test/java/com/zyx/test/ApiTest.java
@@ -0,0 +1,10 @@
+package com.zyx.test;
+
+/**
+ * @author zhangyusen
+ * @version 1.0
+ * @description: 单元测试
+ * @date 2024/5/16 23:08
+ */
+public class ApiTest {
+}
